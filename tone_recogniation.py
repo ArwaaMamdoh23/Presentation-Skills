a79f1f9270f2ed60db1fc56f072650ef7effce5e
Mostafaaelbarbary <126014488+Mostafaaelbarbary@users.noreply.github.com>
2024-11-15 16:39:13 +0200

    Update tone_recogniation.py

--- tone_recogniation.py
+++ tone_recogniation.py
@@ -11,11 +11,12 @@ import seaborn as sns
 from sklearn.model_selection import learning_curve
 
 dataset_path = r"D:\Senior\grad project\tone detection\tone detection"
+
 categories = ['Men_angry', 'Men_Sad', 'Men_neutral', 'Men_fear', 'Men_happy', 
               'Female_angry', 'Female_fear', 'Female_happy', 'Female_sad', 'Female_neutral']
 
 def extract_features(file_path):
-    audio, sr = librosa.load(file_path, sr=None)
+    audio, sr = librosa.load(file_path, sr=22050)
     mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
     mfcc_scaled = np.mean(mfcc.T, axis=0)
     return mfcc_scaled
@@ -24,12 +25,15 @@ def load_data(dataset_path):
     features = []
     labels = []
     label_encoder = LabelEncoder()
+    
     for category in categories:
         file_path = os.path.join(dataset_path, category, '*.wav')
         files = glob.glob(file_path)
+        
         for file in files:
             features.append(extract_features(file))
             labels.append(category)
+    
     features = np.array(features)
     labels = label_encoder.fit_transform(labels)
     return features, labels, label_encoder
@@ -45,9 +49,11 @@ X_val = scaler.transform(X_val)
 X_test = scaler.transform(X_test)
 
 svm_model = SVC(kernel='linear')
+
 svm_model.fit(X_train, y_train)
 
 y_pred = svm_model.predict(X_test)
+
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Test Accuracy: {accuracy * 100:.2f}%")
 
@@ -57,11 +63,24 @@ print(f"Validation Accuracy: {val_accuracy * 100:.2f}%")
 
 def predict_emotion(audio_path, model, scaler, label_encoder):
     feature = extract_features(audio_path)
+    print("Extracted Features:", feature)
     feature_scaled = scaler.transform([feature])
     prediction = model.predict(feature_scaled)
     predicted_label = label_encoder.inverse_transform(prediction)
+    decision = model.decision_function(feature_scaled)
+    print("Decision Function Output:", decision)
     return predicted_label[0]
 
-audio_file_path = r"C:\Users\MOUSTAFA\OneDrive\Documents\GitHub\Presentation-Skills\test\female_fear\YAF_bar_fear.wav"
+audio_file_path = r"C:\Users\MOUSTAFA\OneDrive\Desktop\female_neutral (1).wav"
+
 predicted_emotion = predict_emotion(audio_file_path, svm_model, scaler, label_encoder)
+
 print(f"Predicted Emotion: {predicted_emotion}")
+
+audio, sr = librosa.load(audio_file_path, sr=22050)
+mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(mfcc, x_axis='time')
+plt.colorbar()
+plt.title("MFCC Features of the Test Audio")
+plt.show()
