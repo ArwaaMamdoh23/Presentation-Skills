56efba4a6a128324ff5da5507ae9be80373bda4d
mayaradel03 <125468719+mayaradel03@users.noreply.github.com>
2025-03-11 05:41:19 +0200

    Home

--- my_flutter_project/lib/Screens/HomePage.dart
+++ my_flutter_project/lib/Screens/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 import '../widgets/custom_app_bar.dart'; // Import Custom AppBar
 import '../widgets/background_wrapper.dart'; // ✅ Import the wrapper
+import '../widgets/CustomDrawer .dart'; 
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,7 +12,11 @@ class HomePage extends StatelessWidget {
     bool isUserSignedIn = false; // Simulate user authentication
 
     return Scaffold(
-      appBar: CustomAppBar(showSignIn: true, isUserSignedIn: isUserSignedIn), // ✅ Show Sign In button only here
+      appBar: CustomAppBar(
+        showSignIn: !isUserSignedIn,
+        isUserSignedIn: isUserSignedIn
+        ), 
+      drawer: isUserSignedIn ? CustomDrawer(isSignedIn: isUserSignedIn) : null,
       backgroundColor: Colors.transparent,
       extendBodyBehindAppBar: true,
       body: BackgroundWrapper( // ✅ Wrap the entire page content
