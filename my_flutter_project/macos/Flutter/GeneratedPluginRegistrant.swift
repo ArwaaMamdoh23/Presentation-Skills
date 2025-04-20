c7ed542d45de881c24413c3f4845d30c5daefc4a
ArwaaMamdoh23 <arwaa2110478@miuegypt.edu.eg>
2025-04-20 02:06:51 +0200

    commit

--- my_flutter_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ my_flutter_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,8 @@ import app_links
 import connectivity_plus
 import file_picker
 import file_selector_macos
+import flutter_secure_storage_macos
+import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -18,6 +20,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
