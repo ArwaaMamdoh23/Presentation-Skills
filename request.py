967f980bd55618d2a0a0e1ad76fec63bf423f14c
ArwaaMamdoh23 <arwaa2110478@miuegypt.edu.eg>
2025-05-04 15:48:18 +0300

    c

--- request.py
+++ request.py
@@ -1,10 +1,15 @@
 import requests
 
 # Open the video file to send with the request
-files = {'video': open('Videos/TedTalk.mp4', 'rb')}
+with open('Videos/TedTalk.mp4', 'rb') as f:
+    files = {'video': f}
 
-# Make the POST request to upload the video
-response = requests.post('http://127.0.0.1:5000/upload_video', files=files)
+    # Make the POST request to upload the video
+    response = requests.post('http://127.0.0.1:5000/upload_video', files=files)
 
-# Print the response from the server
-print(response.json())  # This should print the feedback response
+    # Print the response from the server
+    if response.status_code == 200:
+        print("Success:", response.json())  # This should print the feedback response
+    else:
+        print("Failed to upload video. Status code:", response.status_code)
+        print(response.text)
